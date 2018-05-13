5ea674fdf3e9f5d3a2a46474677e521b4d68ba90
root <you@example.com>
2018-05-13 12:52:22 +0100

    create function to sanitize inputs coming from both the login and sign up form strip tags, remove empty strings and capitilise username and names

--- register.php
+++ register.php
@@ -1,3 +1,50 @@
+<?php
+
+  if (isset($_POST['loginButton'])) {
+
+  }
+
+  // sanitise unername and emails
+  function sanitizeFormInput ($inputText)  {
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ", "", $inputText);
+    return $inputText;
+  }
+  // sanitise string inputs
+  function sanitiseStrings ($inputString) {
+    $inputString = strip_tags($inputString);
+    $inputString = str_replace(" ", "", $inputString);
+    $inputString = ucfirst(strtolower($inputString));
+    return $inputString;
+  }
+  // sanitise passwords
+  function sanitisepassword ($password) {
+    $password = strip_tags($password);
+    return $password;
+  }
+
+  if (isset($_POST['registerButton'])) {
+    // sanitise all $input
+    // usernames
+    $username = sanitizeFormInput($_POST['loginUsername']);
+    $username = sanitizeFormInput($_POST['RegisterUsername']);
+    // first and last names
+    $firstName = sanitiseStrings($_POST['RegisterFirstName']);
+    $lastName = sanitiseStrings($_POST['RegisterLastName']);
+    // emails
+    $email = sanitizeFormInput($_POST['RegisterEmail']);
+    $confirmEmail = sanitizeFormInput($_POST['confirmRegisterEmail']);
+    // passwords
+    $password = sanitisepassword($_POST['password']);
+    $confirmPassword = sanitisepassword($_POST['confirmPassword']);
+  }
+
+
+
+
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -7,8 +54,8 @@
 
   <body>
 
-    <form id="inputContainer" action="register.php" method="post">
-      <h2>Login to your fred-stream account</h2>
+    <form id="inputContainer" action="register.php" method="POST">
+      <h2>Login to your Fredify account</h2>
       <p>
         <label for="loginUsername">Username</label>
         <input type="text" name="loginUsername" placeholder="username" required>
@@ -17,40 +64,40 @@
         <label for="loginPassword">Password</label>
         <input type="password" name="loginPassword" placeholder="password" required>
       </p>
-      <button type="submit" name="button">Log in</button>
+      <button type="submit" name="loginButton">Log in</button>
     </form>
 
-    <form id="inputContainer" action="register.php" method="post">
+    <form id="inputContainer" action="register.php" method="POST">
       <h2>Create your free account</h2>
       <p>
-        <label for="loginUsername">Username</label>
-        <input type="text" name="loginUsername" placeholder="e.g janedow401" required>
+        <label for="RegisterUsername">Username</label>
+        <input type="text" name="RegisterUsername" placeholder="e.g janedow401" required>
       </p>
       <p>
-        <label for="loginFirstName">First name</label>
-        <input type="text" name="loginFirstName" placeholder="e.g Jane" required>
+        <label for="RegisterFirstName">First name</label>
+        <input type="text" name="RegisterFirstName" placeholder="e.g Jane" required>
       </p>
       <p>
-        <label for="loginLastName">Last name</label>
-        <input type="text" name="loginLastName" placeholder="e.g Dow" required>
+        <label for="RegisterLastName">Last name</label>
+        <input type="text" name="RegisterLastName" placeholder="e.g Dow" required>
       </p>
       <p>
-        <label for="loginEmail">Email</label>
-        <input type="email" name="loginEmail" placeholder="e.g janedow@gmail.com" required>
+        <label for="RegisterEmail">Email</label>
+        <input type="email" name="RegisterEmail" placeholder="e.g janedow@gmail.com" required>
       </p>
       <p>
-        <label for="confirmLoginEmail">Confirm email</label>
-        <input type="email" name="confirmLoginEmail" placeholder="e.g janedow@gmail.com" required>
+        <label for="confirmRegisterEmail">Confirm email</label>
+        <input type="email" name="confirmRegisterEmail" placeholder="e.g janedow@gmail.com" required>
       </p>
       <p>
-        <label for="loginPassword">Password</label>
-        <input type="password" name="loginPassword" placeholder="password" required>
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="password" required>
       </p>
       <p>
-        <label for="confirmLoginPassword">Password</label>
-        <input type="password" name="confirmLoginPassword" placeholder="password" required>
+        <label for="confirmPassword">Password</label>
+        <input type="password" name="confirmPassword" placeholder="password" required>
       </p>
-      <button type="submit" name="button">Log in</button>
+      <button type="submit" name="registerButton">Log in</button>
     </form>
   </body>
 
