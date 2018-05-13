69b8f3441ea9e04bc6cdecf6df042716169ede06
root <you@example.com>
2018-05-13 13:04:51 +0100

    refactor my code - moved all my php codes into a separate file--cleaner code

--- register.php
+++ register.php
@@ -1,48 +1,6 @@
 <?php
-
-  if (isset($_POST['loginButton'])) {
-
-  }
-
-  // sanitise unername and emails
-  function sanitizeFormInput ($inputText)  {
-    $inputText = strip_tags($inputText);
-    $inputText = str_replace(" ", "", $inputText);
-    return $inputText;
-  }
-  // sanitise string inputs
-  function sanitiseStrings ($inputString) {
-    $inputString = strip_tags($inputString);
-    $inputString = str_replace(" ", "", $inputString);
-    $inputString = ucfirst(strtolower($inputString));
-    return $inputString;
-  }
-  // sanitise passwords
-  function sanitisepassword ($password) {
-    $password = strip_tags($password);
-    return $password;
-  }
-
-  if (isset($_POST['registerButton'])) {
-    // sanitise all $input
-    // usernames
-    $username = sanitizeFormInput($_POST['loginUsername']);
-    $username = sanitizeFormInput($_POST['RegisterUsername']);
-    // first and last names
-    $firstName = sanitiseStrings($_POST['RegisterFirstName']);
-    $lastName = sanitiseStrings($_POST['RegisterLastName']);
-    // emails
-    $email = sanitizeFormInput($_POST['RegisterEmail']);
-    $confirmEmail = sanitizeFormInput($_POST['confirmRegisterEmail']);
-    // passwords
-    $password = sanitisepassword($_POST['password']);
-    $confirmPassword = sanitisepassword($_POST['confirmPassword']);
-  }
-
-
-
-
-
+  include ("includes/handlers/register-handler.php");
+  include ("includes/handlers/login-handler.php");
  ?>
 
 <!DOCTYPE html>
