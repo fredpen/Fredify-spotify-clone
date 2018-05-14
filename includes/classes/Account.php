aac08f99110f7f392af4b948f94147b1a0576747
root <you@example.com>
2018-05-14 08:11:10 +0100

    create the private function to validate the firstname, lastname, and the emails the vaidation of email serves as the backup for the front end check using the email input type

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -27,14 +27,29 @@
       }
       // firstname authentication
       private function  validateFirstname($firstName) {
+        if (strlen($firstName) > 25 || strlen($firstName) < 2) {
+          array_push($this->errorArray, "Your First name must be between 2 and 25 characters");
+          return;
+        }
       }
       // lastname authentication
       private function validateLastname ($lastName) {
+        if (strlen($lastName) > 25 || strlen($lastName) < 2) {
+          array_push($this->errorArray, "Your last name must be betwwen 2 and 25 characters");
+          return;
+        }
 
       }
       // emails authentication
       private function validateEmails ($email, $confirmEmail) {
-
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          array_push($this->errorArray, "Email is invalid");
+          return;
+        }
+        if ($email != $confirmEmail) {
+          array_push($this->errorArray, "Your Emails do not match")
+        }
+        //check if the email already exists
       }
 
       // emaipasswords authentication
