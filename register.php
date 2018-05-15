896be0bbd183b2d05f0910f15c92fb240a3bf1fc
root <you@example.com>
2018-05-15 06:55:35 +0100

    styling of the text boxes styling of the input placeholder styling of the button

--- register.php
+++ register.php
@@ -23,67 +23,70 @@ function getInputValue ($name) {
     <meta charset="utf-8">
     <title>Welcome to Fredify!</title>
     <link rel="stylesheet" href="assets/css/register.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
   </head>
 
   <body>
     <div id="background">
       <div id="loginContainer">
-         <form id="inputContainer" action="register.php" method="POST">
-          <h2>Login to your Fredify account</h2>
-          <p>
-            <label for="loginUsername">Username</label>
-            <input type="text" name="loginUsername" placeholder="username" required>
-          </p>
-          <p>
-            <label for="loginPassword">Password</label>
-            <input type="password" name="loginPassword" placeholder="password" required>
-          </p>
-          <button type="submit" name="loginButton">Log in</button>
-        </form>
+        <div id="inputContainer">
+           <form class="inputContainer" action="register.php" method="POST">
+            <h2>Login to your Fredify account</h2>
+            <p>
+              <label for="loginUsername">Username</label>
+              <input type="text" name="loginUsername" placeholder="e.g janedow401" required>
+            </p>
+            <p>
+              <label for="loginPassword">Password</label>
+              <input type="password" name="loginPassword" placeholder="password" required>
+            </p>
+            <button type="submit" name="loginButton">Log in</button>
+          </form>
 
-        <!-- =============================================== -->
-        <form id="inputContainer" action="register.php" method="POST">
-          <h2>Create your free account</h2>
-          <p>
-            <?php echo $account->getError(Constants::$usernameCharacter) ?>
-            <label for="username">Username</label>
-            <input type="text" value="<?php getInputValue('username') ?>" name="username" placeholder="e.g janedow401" required>
-          </p>
-          <p>
-            <?php echo $account->getError(Constants::$fistnameCharacter) ?>
-            <label for="firstname">First name</label>
-            <input type="text" value="<?php getInputValue('firstname') ?>" name="firstname" placeholder="e.g Jane" required>
-          </p>
-          <p>
-            <?php echo $account->getError(Constants::$lastnameCharacter) ?>
-            <label for="lastname">Last name</label>
-            <input type="text" name="lastname" value="<?php getInputValue('lastname') ?>" placeholder="e.g Dow" required>
-          </p>
-          <p>
-            <?php echo $account->getError(Constants::$emailInvalid);
-                    echo $account->getError(Constants::$emailDoNotMatch);
-              ?>
-            <label for="email">Email</label>
-            <input type="email" name="email" value="<?php getInputValue('email') ?>" placeholder="e.g janedow@gmail.com" required>
-          </p>
-          <p>
-            <label for="confirmEmail">Confirm email</label>
-            <input type="email" name="confirmEmail" value="<?php getInputValue('confirmEmail') ?>" placeholder="e.g janedow@gmail.com" required>
-          </p>
-          <p>
-            <?php echo $account->getError(Constants::$passwordInvalid);
-                    echo $account->getError(Constants::$passwordCharacter);
-                    echo $account->getError(Constants::$passwordDonNotMatch);
-              ?>
-            <label for="password">Password</label>
-            <input type="password" name="password" placeholder="password" required>
-          </p>
-          <p>
-            <label for="confirmPassword">Password</label>
-            <input type="password" name="confirmPassword" placeholder="password" required>
-          </p>
-          <button type="submit" name="registerButton">Log in</button>
-        </form>
+          <!-- =============================================== -->
+          <form class="inputContainer" action="register.php" method="POST">
+            <h2>Create your free account</h2>
+            <p>
+              <?php echo $account->getError(Constants::$usernameCharacter) ?>
+              <label for="username">Username</label>
+              <input type="text" value="<?php getInputValue('username') ?>" name="username" placeholder="e.g janedow401" required>
+            </p>
+            <p>
+              <?php echo $account->getError(Constants::$fistnameCharacter) ?>
+              <label for="firstname">First name</label>
+              <input type="text" value="<?php getInputValue('firstname') ?>" name="firstname" placeholder="e.g Jane" required>
+            </p>
+            <p>
+              <?php echo $account->getError(Constants::$lastnameCharacter) ?>
+              <label for="lastname">Last name</label>
+              <input type="text" name="lastname" value="<?php getInputValue('lastname') ?>" placeholder="e.g Dow" required>
+            </p>
+            <p>
+              <?php echo $account->getError(Constants::$emailInvalid);
+                      echo $account->getError(Constants::$emailDoNotMatch);
+                ?>
+              <label for="email">Email</label>
+              <input type="email" name="email" value="<?php getInputValue('email') ?>" placeholder="e.g janedow@gmail.com" required>
+            </p>
+            <p>
+              <label for="confirmEmail">Confirm email</label>
+              <input type="email" name="confirmEmail" value="<?php getInputValue('confirmEmail') ?>" placeholder="e.g janedow@gmail.com" required>
+            </p>
+            <p>
+              <?php echo $account->getError(Constants::$passwordInvalid);
+                      echo $account->getError(Constants::$passwordCharacter);
+                      echo $account->getError(Constants::$passwordDonNotMatch);
+                ?>
+              <label for="password">Password</label>
+              <input type="password" name="password" placeholder="password" required>
+            </p>
+            <p>
+              <label for="confirmPassword">Password</label>
+              <input type="password" name="confirmPassword" placeholder="password" required>
+            </p>
+            <button type="submit" name="registerButton">Log in</button>
+          </form>
+        </div>
       </div>
     </div>
   </body>
