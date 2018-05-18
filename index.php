1298fb5d7e49e3b6b7d76dffb3a14dfd7b3d929c
root <you@example.com>
2018-05-18 22:55:38 +0100

    Applying styles to the top container postioning the navBarContainer fixed on the left..for a static positioning

--- index.php
+++ index.php
@@ -18,77 +18,85 @@
     <link rel="stylesheet" href="assets/css/styles.css">
   </head>
   <body>
-    <div id="nowPlayingBarCointaner">
-      <div id="nowPlayingBar">
-        <!-- the first section the houses the album work -->
-        <div id="nowPlayingIcon">
-          <div class="content">
-            <div class="albumLink">
-              <img class="albumartwork" src="assets/images/icons/marley.jpeg" alt="albumartwork">
-            </div>
-            <div class="trackInfo">
-              <span class="trackName">
-                <span>Happy Birthday</span>
-              </span>
-              <span class="artistName">
-                <span>fred</span>
-              </span>
+    <div id="mainContainer">
+      <div id="topContainer">
+        <div id="NavBarContainer">
+          
+        </div>
+      </div>
+      <div id="nowPlayingBarCointaner">
+        <div id="nowPlayingBar">
+          <!-- the first section the houses the album work -->
+          <div id="nowPlayingIcon">
+            <div class="content">
+              <div class="albumLink">
+                <img class="albumartwork" src="assets/images/icons/marley.jpeg" alt="albumartwork">
+              </div>
+              <div class="trackInfo">
+                <span class="trackName">
+                  <span>Happy Birthday</span>
+                </span>
+                <span class="artistName">
+                  <span>fred</span>
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-        <!-- the midlle section of the playbar -->
-        <div id="nowPlayingButtons">
+          <!-- the midlle section of the playbar -->
+          <div id="nowPlayingButtons">
 
-          <div class="content playerControls">
-            <div class="butons">
-              <button class="controlButton shuffle" title="Shuffle">
-                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-              </button>
-              <button class="controlButton previous" title="previous">
-                <img src="assets/images/icons/previous.png" alt="previous">
-              </button>
-              <button class="controlButton play" title="play">
-                <img src="assets/images/icons/play.png" alt="play">
-              </button>
-              <button class="controlButton pause" title="pause">
-                <img src="assets/images/icons/pause.png" alt="pause">
-              </button>
-              <button class="controlButton next" title="next">
-                <img src="assets/images/icons/next.png" alt="next">
-              </button>
-              <button class="controlButton repeat" title="repeat">
-                <img src="assets/images/icons/repeat.png" alt="repeat">
-              </button>
+            <div class="content playerControls">
+              <div class="butons">
+                <button class="controlButton shuffle" title="Shuffle">
+                  <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                </button>
+                <button class="controlButton previous" title="previous">
+                  <img src="assets/images/icons/previous.png" alt="previous">
+                </button>
+                <button class="controlButton play" title="play">
+                  <img src="assets/images/icons/play.png" alt="play">
+                </button>
+                <button class="controlButton pause" title="pause">
+                  <img src="assets/images/icons/pause.png" alt="pause">
+                </button>
+                <button class="controlButton next" title="next">
+                  <img src="assets/images/icons/next.png" alt="next">
+                </button>
+                <button class="controlButton repeat" title="repeat">
+                  <img src="assets/images/icons/repeat.png" alt="repeat">
+                </button>
+              </div>
+
+              <div class="playBackBar">
+                  <span class="progressTime current">0:00</span>
+                  <div class="progressbar">
+                    <div class="progressbarBg">
+                      <div class="progress"></div>
+                    </div>
+                  <span class="progressTime remaining">0:00</span>
+              </div>
             </div>
-
-            <div class="playBackBar">
-                <span class="progressTime current">0:00</span>
+          </div>
+          </div>
+          <!-- the third section that house the volume bar -->
+          <div id="nowPlayingVolume">
+            <div class="volumeBar">
+              <div class="butons">
+                <button class="controlButton volume" title="Volume">
+                  <img src="assets/images/icons/volume.png" alt="Volume button">
+                </button>
                 <div class="progressbar">
                   <div class="progressbarBg">
                     <div class="progress"></div>
                   </div>
-                <span class="progressTime remaining">0:00</span>
-            </div>
-          </div>
-        </div>
-        </div>
-        <!-- the third section that house the volume bar -->
-        <div id="nowPlayingVolume">
-          <div class="volumeBar">
-            <div class="butons">
-              <button class="controlButton volume" title="Volume">
-                <img src="assets/images/icons/volume.png" alt="Volume button">
-              </button>
-              <div class="progressbar">
-                <div class="progressbarBg">
-                  <div class="progress"></div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <!-- middle section ends here -->
-      </div> <!--now playing bar -->
-    </div><!--now playing container -->
+          <!-- middle section ends here -->
+        </div> <!--now playing bar -->
+      </div><!--now playing container -->
+    </div>
+
   </body>
 </html>
