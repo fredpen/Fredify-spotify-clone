3d70dc6672e398e521470928c1654b3c58460f80
root <you@example.com>
2018-05-18 22:31:45 +0100

    finalizing the stylings on the now playing bar

--- index.php
+++ index.php
@@ -17,30 +17,30 @@
     <link rel="shortcut icon" href="assets/images/icons/favicon.jpg">
     <link rel="stylesheet" href="assets/css/styles.css">
   </head>
-
   <body>
     <div id="nowPlayingBarCointaner">
-
       <div id="nowPlayingBar">
-
+        <!-- the first section the houses the album work -->
         <div id="nowPlayingIcon">
-
           <div class="content">
-
-            <span class="albumLink">
+            <div class="albumLink">
               <img class="albumartwork" src="assets/images/icons/marley.jpeg" alt="albumartwork">
-            </span>
-
+            </div>
+            <div class="trackInfo">
+              <span class="trackName">
+                <span>Happy Birthday</span>
+              </span>
+              <span class="artistName">
+                <span>fred</span>
+              </span>
+            </div>
           </div>
-
         </div>
-
+        <!-- the midlle section of the playbar -->
         <div id="nowPlayingButtons">
 
           <div class="content playerControls">
-
             <div class="butons">
-
               <button class="controlButton shuffle" title="Shuffle">
                 <img src="assets/images/icons/shuffle.png" alt="Shuffle">
               </button>
@@ -59,38 +59,36 @@
               <button class="controlButton repeat" title="repeat">
                 <img src="assets/images/icons/repeat.png" alt="repeat">
               </button>
-
             </div>
 
             <div class="playBackBar">
-
                 <span class="progressTime current">0:00</span>
                 <div class="progressbar">
-
                   <div class="progressbarBg">
-
                     <div class="progress"></div>
-
                   </div>
-
-
-                </div>
                 <span class="progressTime remaining">0:00</span>
             </div>
-
           </div>
-
         </div>
-
+        </div>
+        <!-- the third section that house the volume bar -->
         <div id="nowPlayingVolume">
-
+          <div class="volumeBar">
+            <div class="butons">
+              <button class="controlButton volume" title="Volume">
+                <img src="assets/images/icons/volume.png" alt="Volume button">
+              </button>
+              <div class="progressbar">
+                <div class="progressbarBg">
+                  <div class="progress"></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-
-      </div>
-
-    </div>
-
-    </div>
+        <!-- middle section ends here -->
+      </div> <!--now playing bar -->
+    </div><!--now playing container -->
   </body>
-
 </html>
